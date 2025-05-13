c0d5d4df5c66455a5244986fd11537f8de4228c2
Joshua Ike <79171733+kxng0109@users.noreply.github.com>
2025-05-14 00:30:07 +0100

    Forgot to add helmet.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { ForbiddenFilter, PrismaClientExceptionFilter } from './auth/filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet())
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
